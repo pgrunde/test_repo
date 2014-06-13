e32ce8b43a099ae17178d0e4d2d73d6234c3e7eb
Peter Grunde <peter.a.grunde@gmail.com>
2014-06-12 22:59:04 -0600

    redo sort

---
+++ test.rb
@@ -0,0 +1,26 @@
+array = ["one", "two", "three", "four"]
+
+def sort arr
+  sort_arr arr, []
+end
+
+def sort_arr unsorted, sorted
+  if unsorted.length <= 0
+    return sorted
+  end
+
+  still_unsorted = []
+  smallest = unsorted.pop
+  unsorted.each do |item|
+    if smallest > item
+      still_unsorted.push smallest
+      smallest = item
+    elsif smallest < item
+      still_unsorted.push item
+    end
+  end
+  sorted.push smallest
+  sort_arr still_unsorted, sorted
+end
+
+puts sort(array)
